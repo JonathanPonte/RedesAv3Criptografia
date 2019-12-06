b388a659996787891a48232e0d434d60a959939c
JonathanPonte <jonathancardoso2@hotmail.com>
2019-12-06 18:38:57 -0300

    add Cifra de cesar

--- src/Telas/principal.java
+++ src/Telas/principal.java
@@ -21,12 +21,15 @@ import java.io.File;
 import java.io.IOException;
 import java.awt.event.ActionEvent;
 import javax.swing.SwingConstants;
+import javax.swing.JTextField;
 
 public class principal extends JFrame {
 
 	private JPanel contentPane;
 	private BufferedImage img = null;
-	private String  pasta = null;
+	private String pasta = null;
+	private boolean foiClicado = false;
+	private JTextField chave;
 
 	/**
 	 * Launch the application.
@@ -56,20 +59,46 @@ public class principal extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		contentPane.setLayout(null);
+		
+		JLabel mural = new JLabel();
+		mural.setHorizontalAlignment(SwingConstants.CENTER);
+		mural.setBounds(98, 69, 704, 414);
+		contentPane.add(mural);
+		setLocationRelativeTo(null);
+		setResizable(false);
+		
+		
+		chave = new JTextField();
+		chave.setBounds(198, 523, 57, 19);
+		contentPane.add(chave);
+		chave.setColumns(10);
 
-		JButton btnNewButton = new JButton("1");
+		JButton btnNewButton = new JButton("Cifra de cesar");
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				CifraCesar cifraCesar = new CifraCesar();
 				try {
-					//teste
-					cifraCesar.obterMatrizColor(pasta);
+					
+					BufferedImage novaImgCrip = null;
+					
+					if(!foiClicado) {
+						foiClicado = true;
+						novaImgCrip = cifraCesar.converForImage(cifraCesar.obterMatrizColor(pasta, Integer.parseInt(chave.getText()), 1), img.getWidth(), img.getHeight());
+						mural.setIcon(new ImageIcon(novaImgCrip));
+					}else {
+						foiClicado = false;
+						novaImgCrip = cifraCesar.converForImage(cifraCesar.obterMatrizColor(pasta, Integer.parseInt(chave.getText()), 0), img.getWidth(), img.getHeight());
+						mural.setIcon(new ImageIcon(novaImgCrip));
+					}
+					
+					
+					
 				} catch (IOException e1) {
 					System.out.println("deu ruim no cesar");
 				}
 			}
 		});
-		btnNewButton.setBounds(36, 520, 114, 25);
+		btnNewButton.setBounds(36, 520, 151, 25);
 		contentPane.add(btnNewButton);
 
 		JButton btnNewButton_1 = new JButton("2");
@@ -80,19 +109,14 @@ public class principal extends JFrame {
 		btnNewButton_2.setBounds(743, 520, 114, 25);
 		contentPane.add(btnNewButton_2);
 
-		JLabel mural = new JLabel();
-		mural.setHorizontalAlignment(SwingConstants.CENTER);
-		mural.setBounds(98, 69, 704, 414);
-		contentPane.add(mural);
-		setLocationRelativeTo(null);
-		setResizable(false);
+		
 
 		JButton btnNewButton_3 = new JButton("Adicionar imagem");
 		btnNewButton_3.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				JFileChooser fileChooser = new JFileChooser();
 				int returnValor = fileChooser.showOpenDialog(fileChooser);
-				//String pasta = null;
+				// String pasta = null;
 				if (returnValor == JFileChooser.APPROVE_OPTION) {
 					pasta = fileChooser.getSelectedFile().getAbsolutePath();
 					try {
@@ -101,21 +125,19 @@ public class principal extends JFrame {
 						// TODO: handle exception
 					}
 					Image novaImg = null;
-					if(img.getHeight() > 800) {
+					if (img.getHeight() > 800) {
 						novaImg = img.getScaledInstance(800, img.getWidth(), Image.SCALE_DEFAULT);
 					}
-					
-					if(img.getWidth() > 500) {
+
+					if (img.getWidth() > 500) {
 						novaImg = img.getScaledInstance(img.getHeight(), 400, Image.SCALE_DEFAULT);
 					}
-					
-					
-					if(novaImg == null) {
+
+					if (novaImg == null) {
 						mural.setIcon(new ImageIcon(img));
-					}else {
+					} else {
 						mural.setIcon(new ImageIcon(novaImg));
 					}
-					
 
 				} else {
 					System.out.println("Deu ruim");
@@ -125,6 +147,11 @@ public class principal extends JFrame {
 		});
 		btnNewButton_3.setBounds(36, 12, 165, 25);
 		contentPane.add(btnNewButton_3);
-
+		
+		
+		
+		JLabel lblChave = new JLabel("Chave");
+		lblChave.setBounds(198, 505, 66, 15);
+		contentPane.add(lblChave);
 	}
 }
--- src/algoritimos/CifraCesar.java
+++ src/algoritimos/CifraCesar.java
@@ -8,23 +8,80 @@ import java.io.IOException;
 import javax.imageio.ImageIO;
 
 public class CifraCesar {
-	public Color[][] obterMatrizColor(String caminho) throws IOException {
+
+	public Color[][] obterMatrizColor(String caminho, int chaveCrip, int incrip) throws IOException {
 		BufferedImage bufferedImage = ImageIO.read(new File(caminho));
-		
+
+		System.out.println("Primeiro" + bufferedImage.getWidth() + "-" + bufferedImage.getHeight());
+
 		Integer largura = bufferedImage.getWidth();
 		Integer altura = bufferedImage.getHeight();
-		
+
 		Color[][] pixel = new Color[largura][altura];
-		for(int i = 0 ; i < largura; i++) {
-			for(int j = 0; j < altura; j++) {
+		for (int i = 0; i < largura; i++) {
+			for (int j = 0; j < altura; j++) {
 				Integer cor = bufferedImage.getRGB(i, j);
 				Integer r = (cor & 0x00ff0000) >> 16;
 				Integer g = (cor & 0x0000ff00) >> 8;
 				Integer b = cor & 0x000000ff;
-				pixel[i][j] = new Color(r, g, b);
+				
+				if(incrip == 1) {
+					pixel[i][j] = cifraCesar(r, g, b, chaveCrip);
+				}else {
+					pixel[i][j] = descriptografar(r, g, b, chaveCrip);
+				}
+				
 			}
 		}
-		
+
 		return pixel;
 	}
+
+	public Color cifraCesar(Integer r, Integer g, Integer b, int chavCrip) {
+
+		r = (r + chavCrip) % 256;
+		g = (g + chavCrip) % 256;
+		b = (b + chavCrip) % 256;
+
+		return new Color(r, g, b);
+	}
+
+	public Color descriptografar(Integer r, Integer g, Integer b, int chavCrip) {
+
+		r = r - chavCrip;
+		g = g - chavCrip;
+		b = b - chavCrip;
+
+		if (r < 0) {
+			r += 256;
+		}
+
+		if (g < 0) {
+			g += 256;
+		}
+
+		if (b < 0) {
+			b += 256;
+		}
+
+		return new Color(r, g, b);
+	}
+
+	public BufferedImage converForImage(Color[][] pixel, int largura, int altura) {
+		BufferedImage img = null;
+		img = new BufferedImage(largura, altura, BufferedImage.TYPE_INT_ARGB);
+
+		System.out.println(largura + "-" + altura);
+
+		for (int i = 0; i < largura; i++) {
+			for (int j = 0; j < altura; j++) {
+
+				img.setRGB(i, j, pixel[i][j].getRGB());
+
+			}
+		}
+
+		return img;
+	}
+
 }
